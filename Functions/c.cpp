f1f40c4e69426f8cdeccb3106eb14a875336cf0d
PRANSHU SHARMA <pranshusharma5136@gmail.com>
2025-12-30 22:33:22 +0530

    a to A

---
+++ Functions/c.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+char convert (char name)
+{
+        char ans= name-'a'+'A';
+        return ans;
+}
+
+int main() {
+	char name;
+	cin>>name;
+	cout<<convert(name);
+
+}
