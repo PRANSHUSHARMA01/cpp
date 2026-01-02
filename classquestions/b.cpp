653ca97f0c241832daa2f2f3a9fc325e044820de
PRANSHU SHARMA <pranshusharma5136@gmail.com>
2026-01-02 21:48:32 +0530

    com

---
+++ classquestions/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	float k,n,m;
+	cin>>k>>n>>m;
+	float cal=(k+n)/2;
+	if(cal>m){
+	        cout<<"yes";
+	}
+	else{
+	        cout<<"no";
+	}
+	
+
+}
